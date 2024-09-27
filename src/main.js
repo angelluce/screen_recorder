d4f9c2bdd3907c42df15e7b07fba335af8ec5c93
angelluce <angelluce244@gmail.com>
2024-09-26 20:41:28 -0500

    refactoring code

--- src/main.js
+++ src/main.js
@@ -1,28 +1,55 @@
 const button = document.getElementById('start');
+const toast = document.getElementById('toast');
+let isRecording = false;
+
+showToast('Hola, bienvenido :)');
 
 button.addEventListener('click', async () => {
-    const media = await navigator.mediaDevices.getDisplayMedia({
-        video: { frameRate: { ideal: 10 } },
-        audio: true
-    });
+    if (isRecording) {
+        showToast('Ya hay una grabación activa');
+        return;
+    }
 
-    const recorder = new MediaRecorder(media, {
-        mimeType: 'video/webm; codecs=vp8, opus'
-    });
+    try {
+        const media = await navigator.mediaDevices.getDisplayMedia({
+            video: { frameRate: { ideal: 10 } },
+            audio: true
+        });
 
-    recorder.start();
+        const recorder = new MediaRecorder(media, {
+            mimeType: 'video/webm; codecs=vp8, opus'
+        });
 
-    const [video] = media.getVideoTracks();
+        recorder.start();
+        isRecording = true;
 
-    video.addEventListener('ended', () => {
-        recorder.stop();
-    });
+        const [video] = media.getVideoTracks();
 
-    recorder.addEventListener('dataavailable', (e) => {
-        const url = document.createElement('a');
-        url.href = URL.createObjectURL(e.data);
-        url.download = 'video.webm';
-        url.click();
-    });
+        video.addEventListener('ended', () => {
+            recorder.stop();
+        });
 
+        recorder.addEventListener('dataavailable', (e) => {
+            const url = document.createElement('a');
+            url.href = URL.createObjectURL(e.data);
+            url.download = 'video.webm';
+            url.click();
+        });
+
+        recorder.addEventListener('stop', () => {
+            isRecording = false;
+        });
+    } catch (err) {
+        showToast('No se ha iniciado la grabación');
+        // console.error('Error al iniciar la grabación:', err);
+        isRecording = false;
+    }
 });
+
+function showToast(message) {
+    toast.textContent = message;
+    toast.classList.add('show');
+    setTimeout(() => {
+        toast.classList.remove('show');
+    }, 3000);
+}
